020321f40abd51d8bef83fdab541e4903b4f340a
Chu Lin Tian <chulin.tian.2021@scis.smu.edu.sg>
2023-02-12 03:12:06 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  transformIgnorePatterns: ['/node_modules/(?!lib-to-transform|other-lib)']
 })
